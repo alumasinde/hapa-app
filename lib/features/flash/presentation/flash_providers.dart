4dc1b221030fba0e80ee9d262bf58a55eb586040
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-05 01:10:36 +0300

    Remove unused flash model import

--- lib/features/flash/presentation/flash_providers.dart
+++ lib/features/flash/presentation/flash_providers.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import '../../categories/presentation/category_providers.dart';
 import '../data/flash_repository.dart';
-import '../domain/flash.dart';
 
 final flashRepositoryProvider = Provider<FlashRepository>(
   (ref) => FlashRepository(ref.watch(apiClientProvider)),
