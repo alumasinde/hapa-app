27df1a64c19bf0bba274ae17064cbe1be2b15365
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-04 22:30:10 +0300

    Fix Phase 2 provider wiring

--- lib/features/flash/presentation/flash_providers.dart
+++ lib/features/flash/presentation/flash_providers.dart
@@ -1,6 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../core/network/api_client.dart';
+import '../../categories/presentation/category_providers.dart';
 import '../data/flash_repository.dart';
 import '../domain/flash.dart';
-final flashRepositoryProvider=Provider((ref)=>FlashRepository(ref.watch(apiClientProvider)));
-final nearbyFlashesProvider=FutureProvider<List<Flash>>((ref)=>ref.watch(flashRepositoryProvider).nearby());
+
+final flashRepositoryProvider = Provider<FlashRepository>(
+  (ref) => FlashRepository(ref.watch(apiClientProvider)),
+);
+
+final nearbyFlashesProvider = FutureProvider<List<Flash>>(
+  (ref) => ref.watch(flashRepositoryProvider).nearby(),
+);
