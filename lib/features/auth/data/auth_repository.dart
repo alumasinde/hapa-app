9e17b388294ded9848317afd1adb9502ea2fa73f
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-04 21:51:53 +0300

    Add Phase 1 authentication foundation

---
+++ lib/features/auth/data/auth_repository.dart
@@ -0,0 +1,52 @@
+import '../../../core/network/api_client.dart';
+import '../domain/auth_tokens.dart';
+import '../domain/user.dart';
+
+class AuthSession {
+  const AuthSession({required this.tokens, required this.user});
+  final AuthTokens tokens;
+  final User user;
+}
+
+class AuthRepository {
+  AuthRepository(this._api);
+  final ApiClient _api;
+
+  Future<AuthSession> login({required String email, required String password}) async {
+    final data = await _api.post('/auth/login', {'email': email, 'password': password});
+    return _sessionFrom(data);
+  }
+
+  Future<AuthSession> register({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String phone,
+    required String password,
+  }) async {
+    final data = await _api.post('/auth/register', {
+      'first_name': firstName,
+      'last_name': lastName,
+      'email': email,
+      'phone': phone,
+      'password': password,
+    });
+    return _sessionFrom(data);
+  }
+
+  Future<User> me() async {
+    final data = await _api.get('/me');
+    final user = data['user'] is Map ? Map<String, dynamic>.from(data['user'] as Map) : data;
+    return User.fromJson(user);
+  }
+
+  AuthSession _sessionFrom(Map<String, dynamic> data) {
+    final tokenData = data['tokens'] is Map
+        ? Map<String, dynamic>.from(data['tokens'] as Map)
+        : data;
+    final userData = data['user'] is Map
+        ? Map<String, dynamic>.from(data['user'] as Map)
+        : <String, dynamic>{};
+    return AuthSession(tokens: AuthTokens.fromJson(tokenData), user: User.fromJson(userData));
+  }
+}
